515f1c7bc8648631a384bd7e8071ded2cd71e48d
abhishekraj01432-code <abhishekraj01432@gmal.com>
2026-08-31 10:25:37 +0530

    react introduction

---
+++ REACT-VITE/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Icard from './Components/Icard'
+
+function App() {
+
+  return (
+    <div>
+      <Icard/>
+      <Icard/>
+      <Icard/>
+      <Icard/>
+    </div>
+  )
+}
+
+export default App
